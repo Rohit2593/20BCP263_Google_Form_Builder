20615752b128a32a6c4ab88a7a08be3521a2c525
RohitFlyfin <rohitagarwal@flyfin.org>
2023-08-21 13:16:07 +0530

    testing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Fragment } from "react";
 import FormBuilder from "@components/FormBuilder";
 import "react-nestable/dist/styles/index.css";
+import { Button } from "@material-ui/core";
 const App = () => {
   return (
     <Fragment>
         <FormBuilder />
+        <Button>Submit</>
     </Fragment>
   );
 };
